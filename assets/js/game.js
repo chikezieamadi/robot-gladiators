a04e516df7ca0faecb3b741055de20e51c08a869
Chikezie Amadi <chikezieamadi@gmail.com>
2021-02-20 23:48:00 -0700

    console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert ("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// console.log(playerName);
+// console.log("This is a string, good for leaving yourself a message");
+// console.log(10 + 10);
+// console.log("Our robot's name is " + playerName);
+
+var playerName = "Tony the Robot";
+console.log("Tony the robot " + "is ready for battle!");
+console.log(playerName + " is ready for battle!");
+console.log("Your robot " + playerName + ", has won!");
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun");
+}
+
+//fight();
